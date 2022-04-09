c56582907bed4d357bb4fb67e7e33f3b18fa6a76
Octavian Mitu <octavian.mitu@amdaris.com>
2022-04-09 20:58:40 +0300

    run apps

---
+++ apps/ordering-service/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from ordering service');
+});
+
+const port = process.env.PORT || 3100;
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
